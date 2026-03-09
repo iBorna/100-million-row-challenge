81a7fbd515c1480424961f6aa2a4538525b4a1cf
iBorna <iBorna@users.noreply.github.com>
2026-03-09 18:42:49 +0100

    .

--- app/Parser.php
+++ app/Parser.php
@@ -3,25 +3,22 @@
 namespace App;
 
 use App\Commands\Visit;
-use function array_values;
+use function array_fill;
 use function chr;
 use function count;
 use function fclose;
 use function fgets;
+use function filesize;
 use function fopen;
 use function fread;
 use function fseek;
 use function ftell;
 use function fwrite;
 use function gc_disable;
-use function getmypid;
 use function ini_set;
+use function pack;
 use function pcntl_fork;
 use function pcntl_wait;
-use function shmop_delete;
-use function shmop_open;
-use function shmop_read;
-use function shmop_write;
 use function str_repeat;
 use function str_replace;
 use function stream_set_read_buffer;
@@ -35,9 +32,9 @@ use const SEEK_CUR;
 
 final class Parser
 {
-    private const W = 4;
-    private const CH = 20;
-    private const C = 262_144;
+    private const W = 8;
+    private const CH = 32;
+    private const C = 163_840;
 
     public function parse(string $in, string $out): void
     {
@@ -51,33 +48,29 @@ final class Parser
         $dl = [];
         for ($y = 21; $y <= 26; $y++) {
             for ($m = 1; $m <= 12; $m++) {
-                $md = match ($m) { 2 => $y === 24 ? 29 : 28, 4, 6, 9, 11 => 30, default => 31};
+                $md = match ($m) { 2 => $y === 24 ? 29 : 28, 4, 6, 9, 11 => 30, default => 31 };
                 $ms = ($m < 10 ? '0' : '') . $m;
                 for ($d = 1; $d <= $md; $d++) {
-                    $ds = ($d < 10 ? '0' : '') . $d;
-                    $k = $y . '-' . $ms . '-' . $ds;
+                    $k = "$y-$ms-" . ($d < 10 ? '0' : '') . $d;
                     $db[$k] = $dc;
-                    $dl[$dc++] = '20' . $k;
+                    $dl[$dc++] = $k;
                 }
             }
         }
-
         $nx = [];
         for ($i = 0; $i < 255; $i++)
             $nx[chr($i)] = chr($i + 1);
 
         $fh = fopen($in, 'rb');
         stream_set_read_buffer($fh, 0);
-        $raw = fread($fh, 2_097_152);
-        $lastNl = strrpos($raw, "\n") ?: 0;
-        $nlPad = ($lastNl > 0 && $raw[$lastNl - 1] === "\r") ? 52 : 51;
-
+        $raw = fread($fh, min(2_097_152, $sz));
+        $ln = strrpos($raw, "\n") ?: 0;
+        $nlPad = ($ln > 0 && $raw[$ln - 1] === "\r") ? 52 : 51;
         $pi = [];
         $pl = [];
         $pc = 0;
         $pos = 0;
-        $noNew = 0;
-        while ($pos < $lastNl) {
+        while ($pos < $ln) {
             $nl = strpos($raw, "\n", $pos + 52);
             if ($nl === false)
                 break;
@@ -85,14 +78,10 @@ final class Parser
             if (!isset($pi[$s])) {
                 $pi[$s] = $pc;
                 $pl[$pc++] = $s;
-                $noNew = 0;
-            } elseif (++$noNew > 5000) {
-                break;
             }
             $pos = $nl + 1;
         }
         unset($raw);
-
         foreach (Visit::all() as $v) {
             $s = substr($v->uri, 25);
             if (!isset($pi[$s])) {
@@ -100,7 +89,6 @@ final class Parser
                 $pl[$pc++] = $s;
             }
         }
-
         $pb = [];
         for ($p = 0; $p < $pc; $p++)
             $pb[$pl[$p]] = $p * $dc;
@@ -108,55 +96,95 @@ final class Parser
 
         $bnd = [0];
         for ($i = 1; $i < self::CH; $i++) {
-            fseek($fh, (int) ($sz * $i / self::CH));
+            fseek($fh, (int)($sz * $i / self::CH));
             fgets($fh);
             $bnd[] = ftell($fh);
         }
         fclose($fh);
         $bnd[] = $sz;
+        $totalChunks = self::CH;
 
-        $myPid = getmypid();
-        $shmIds = [];
-        for ($w = 0; $w < self::W - 1; $w++) {
-            $key = (($myPid & 0x3FFF) << 4) | $w;
-            if ($key <= 0)
-                $key = 0x1000 + $w;
-            $shmIds[$w] = shmop_open($key, 'c', 0600, $cells);
-        }
+        $hasSem = function_exists('sem_get');
+        $hasShmop = function_exists('shmop_open');
+        $hasFork = function_exists('pcntl_fork');
 
-        $pidMap = [];
-        for ($w = 0; $w < self::W - 1; $w++) {
-            $pid = pcntl_fork();
-            if ($pid === 0) {
-                gc_disable();
-                $blob = static::crunchWorker($in, $bnd, $w, self::W, $pb, $db, $cells, $nx);
-                shmop_write($shmIds[$w], $blob, 0);
-                exit(0);
+        if ($hasFork && $hasShmop) {
+            $cKey = ftok($in, 'z');
+            $cShm = @shmop_open($cKey, 'c', 0644, 4);
+            shmop_write($cShm, pack('V', 0), 0);
+
+            $sem = $hasSem ? @sem_get(ftok($in, 'y'), 1, 0666, true) : null;
+
+            $rShm = [];
+            for ($w = 0; $w < self::W; $w++)
+                $rShm[$w] = @shmop_open(ftok($in, chr(65 + $w)), 'c', 0644, $cells);
+
+            for ($w = 0; $w < self::W - 1; $w++) {
+                $pid = pcntl_fork();
+                if ($pid === 0) {
+                    gc_disable();
+                    $cnt = str_repeat("\0", $cells);
+                    while (true) {
+                        if ($sem) sem_acquire($sem);
+                        $ci = unpack('V', shmop_read($cShm, 0, 4))[1];
+                        if ($ci >= $totalChunks) {
+                            if ($sem) sem_release($sem);
+                            break;
+                        }
+                        shmop_write($cShm, pack('V', $ci + 1), 0);
+                        if ($sem) sem_release($sem);
+                        static::crunchInto($in, $bnd[$ci], $bnd[$ci + 1], $pb, $db, $nx, $cnt);
+                    }
+                    shmop_write($rShm[$w], $cnt, 0);
+                    exit(0);
+                }
             }
-            $pidMap[$pid] = $w;
-        }
 
-        $baseBlob = static::crunchWorker($in, $bnd, self::W - 1, self::W, $pb, $db, $cells, $nx);
-        $counts = array_values(unpack('C*', $baseBlob));
-        unset($baseBlob);
+            $cnt = str_repeat("\0", $cells);
+            while (true) {
+                if ($sem) sem_acquire($sem);
+                $ci = unpack('V', shmop_read($cShm, 0, 4))[1];
+                if ($ci >= $totalChunks) {
+                    if ($sem) sem_release($sem);
+                    break;
+                }
+                shmop_write($cShm, pack('V', $ci + 1), 0);
+                if ($sem) sem_release($sem);
+                static::crunchInto($in, $bnd[$ci], $bnd[$ci + 1], $pb, $db, $nx, $cnt);
+            }
 
-        $rem = self::W - 1;
-        while ($rem > 0) {
-            $pid = pcntl_wait($st);
-            if (!isset($pidMap[$pid]))
-                continue;
-            $w = $pidMap[$pid];
-            $blob = shmop_read($shmIds[$w], 0, $cells);
+            for ($w = 0; $w < self::W - 1; $w++)
+                pcntl_wait($st);
+
+            $counts = array_fill(0, $cells, 0);
             $j = 0;
-            foreach (unpack('C*', $blob) as $v)
-                $counts[$j++] += $v;
-            shmop_delete($shmIds[$w]);
-            $rem--;
+            foreach (unpack('C*', $cnt) as $v)
+                $counts[$j++] = $v;
+            for ($w = 0; $w < self::W - 1; $w++) {
+                $blob = shmop_read($rShm[$w], 0, $cells);
+                $j = 0;
+                foreach (unpack('C*', $blob) as $v)
+                    $counts[$j++] += $v;
+            }
+
+            for ($w = 0; $w < self::W; $w++) {
+                @shmop_delete($rShm[$w]);
+            }
+            @shmop_delete($cShm);
+            if ($sem) @sem_remove($sem);
+        } else {
+            $cnt = str_repeat("\0", $cells);
+            for ($ci = 0; $ci < $totalChunks; $ci++)
+                static::crunchInto($in, $bnd[$ci], $bnd[$ci + 1], $pb, $db, $nx, $cnt);
+            $counts = array_fill(0, $cells, 0);
+            $j = 0;
+            foreach (unpack('C*', $cnt) as $v)
+                $counts[$j++] = $v;
         }
 
         $dp = [];
         for ($d = 0; $d < $dc; $d++)
-            $dp[$d] = '        "' . $dl[$d] . '": ';
+            $dp[$d] = '        "20' . $dl[$d] . '": ';
         $pp = [];
         for ($p = 0; $p < $pc; $p++)
             $pp[$p] = '"\/blog\/' . str_replace('/', '\/', $pl[$p]) . '"';
@@ -191,38 +219,13 @@ final class Parser
         fclose($o);
     }
 
-    private static function crunchWorker(
-        string $in,
-        array $bnd,
-        int $worker,
-        int $workers,
-        array $pb,
-        array $db,
-        int $cells,
-        array $nx
-    ): string {
-        $cnt = str_repeat("\0", $cells);
-        $chunks = count($bnd) - 1;
-        for ($i = $worker; $i < $chunks; $i += $workers)
-            static::crunchInto($in, $bnd[$i], $bnd[$i + 1], $pb, $db, $nx, $cnt);
-        return $cnt;
-    }
-
-    private static function crunchInto(
-        string $in,
-        int $s,
-        int $e,
-        array $pb,
-        array $db,
-        array $nx,
-        string &$cnt
-    ): void {
+    private static function crunchInto(string $in, int $s, int $e, array $pb, array $db, array $nx, string &$cnt): void
+    {
         $h = fopen($in, 'rb');
         stream_set_read_buffer($h, 0);
         fseek($h, $s);
         $rem = $e - $s;
         $cs = self::C;
-
         while ($rem > 0) {
             $ch = fread($h, $rem > $cs ? $cs : $rem);
             if ($ch === false || $ch === '')
@@ -237,93 +240,75 @@ final class Parser
                 fseek($h, -$t, SEEK_CUR);
                 $rem += $t;
             }
-
             $step = ($ln > 0 && $ch[$ln - 1] === "\r") ? 53 : 52;
             $p = 25;
-            $f = $ln - 2000;
-
+            $f = $ln - 1600;
             while ($p < $f) {
                 $c = strpos($ch, ',', $p);
                 $idx = $pb[substr($ch, $p, $c - $p)] + $db[substr($ch, $c + 3, 8)];
                 $cnt[$idx] = $nx[$cnt[$idx]];
                 $p = $c + $step;
-
                 $c = strpos($ch, ',', $p);
                 $idx = $pb[substr($ch, $p, $c - $p)] + $db[substr($ch, $c + 3, 8)];
                 $cnt[$idx] = $nx[$cnt[$idx]];
                 $p = $c + $step;
-
                 $c = strpos($ch, ',', $p);
                 $idx = $pb[substr($ch, $p, $c - $p)] + $db[substr($ch, $c + 3, 8)];
                 $cnt[$idx] = $nx[$cnt[$idx]];
                 $p = $c + $step;
-
                 $c = strpos($ch, ',', $p);
                 $idx = $pb[substr($ch, $p, $c - $p)] + $db[substr($ch, $c + 3, 8)];
                 $cnt[$idx] = $nx[$cnt[$idx]];
                 $p = $c + $step;
-
                 $c = strpos($ch, ',', $p);
                 $idx = $pb[substr($ch, $p, $c - $p)] + $db[substr($ch, $c + 3, 8)];
                 $cnt[$idx] = $nx[$cnt[$idx]];
                 $p = $c + $step;
-
                 $c = strpos($ch, ',', $p);
                 $idx = $pb[substr($ch, $p, $c - $p)] + $db[substr($ch, $c + 3, 8)];
                 $cnt[$idx] = $nx[$cnt[$idx]];
                 $p = $c + $step;
-
                 $c = strpos($ch, ',', $p);
                 $idx = $pb[substr($ch, $p, $c - $p)] + $db[substr($ch, $c + 3, 8)];
                 $cnt[$idx] = $nx[$cnt[$idx]];
                 $p = $c + $step;
-
                 $c = strpos($ch, ',', $p);
                 $idx = $pb[substr($ch, $p, $c - $p)] + $db[substr($ch, $c + 3, 8)];
                 $cnt[$idx] = $nx[$cnt[$idx]];
                 $p = $c + $step;
-
                 $c = strpos($ch, ',', $p);
                 $idx = $pb[substr($ch, $p, $c - $p)] + $db[substr($ch, $c + 3, 8)];
                 $cnt[$idx] = $nx[$cnt[$idx]];
                 $p = $c + $step;
-
                 $c = strpos($ch, ',', $p);
                 $idx = $pb[substr($ch, $p, $c - $p)] + $db[substr($ch, $c + 3, 8)];
                 $cnt[$idx] = $nx[$cnt[$idx]];
                 $p = $c + $step;
-
                 $c = strpos($ch, ',', $p);
                 $idx = $pb[substr($ch, $p, $c - $p)] + $db[substr($ch, $c + 3, 8)];
                 $cnt[$idx] = $nx[$cnt[$idx]];
                 $p = $c + $step;
-
                 $c = strpos($ch, ',', $p);
                 $idx = $pb[substr($ch, $p, $c - $p)] + $db[substr($ch, $c + 3, 8)];
                 $cnt[$idx] = $nx[$cnt[$idx]];
                 $p = $c + $step;
-
                 $c = strpos($ch, ',', $p);
                 $idx = $pb[substr($ch, $p, $c - $p)] + $db[substr($ch, $c + 3, 8)];
                 $cnt[$idx] = $nx[$cnt[$idx]];
                 $p = $c + $step;
-
                 $c = strpos($ch, ',', $p);
                 $idx = $pb[substr($ch, $p, $c - $p)] + $db[substr($ch, $c + 3, 8)];
                 $cnt[$idx] = $nx[$cnt[$idx]];
                 $p = $c + $step;
-
                 $c = strpos($ch, ',', $p);
                 $idx = $pb[substr($ch, $p, $c - $p)] + $db[substr($ch, $c + 3, 8)];
                 $cnt[$idx] = $nx[$cnt[$idx]];
                 $p = $c + $step;
-
                 $c = strpos($ch, ',', $p);
                 $idx = $pb[substr($ch, $p, $c - $p)] + $db[substr($ch, $c + 3, 8)];
                 $cnt[$idx] = $nx[$cnt[$idx]];
                 $p = $c + $step;
             }
-
             while ($p < $ln) {
                 $c = strpos($ch, ',', $p);
                 if ($c === false || $c >= $ln)
